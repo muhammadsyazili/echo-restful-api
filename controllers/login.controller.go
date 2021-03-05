9e1b49a39fc18233981947da695cf1c531b3572b
muhammadsyazili <09021181520135@students.ilkom.unsri.ac.id>
2021-03-05 23:47:29 +0700

    edit login controller and login model

--- controllers/login.controller.go
+++ controllers/login.controller.go
@@ -1,25 +1,47 @@
 package controllers
 
 import (
-	"net/http"
+	"fmt"
 	"time"
+	"net/http"
+	"database/sql"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo/v4"
 	"github.com/muhammadsyazili/echo-rest/models"
+	"github.com/muhammadsyazili/echo-rest/helpers"
 )
 
 func CheckLogin(c echo.Context) error {
+	var res models.Response
+
 	username := c.FormValue("username")
 	password := c.FormValue("password")
 
-	res, err := models.CheckLogin(username, password)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	result, err := models.CheckLogin(username, password)
+
+	if err == sql.ErrNoRows {
+		fmt.Println("Username not found!")
+
+		res.Status = http.StatusBadRequest
+		res.Message = err.Error()
+		return c.JSON(http.StatusBadRequest, res)
 	}
 
-	if !res {
-		return echo.ErrUnauthorized
+	if err != nil {
+		res.Status = http.StatusInternalServerError
+		res.Message = err.Error()
+
+		return c.JSON(http.StatusInternalServerError, res)
+	}
+
+	match, err := helpers.CheckHash(password, result.Password)
+	if !match {
+		fmt.Println("Password and hash does't match!")
+
+		res.Status = http.StatusBadRequest
+		res.Message = err.Error()
+		return c.JSON(http.StatusBadRequest, res)
 	}
 
 	// Create token
@@ -27,15 +49,21 @@ func CheckLogin(c echo.Context) error {
 
 	// Set claims
 	claims := token.Claims.(jwt.MapClaims)
-	claims["name"] = username
-	claims["level"] = "admin"
+	claims["id"] = result.Id
+	claims["name"] = result.Username
 	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
 
 	// Generate encoded token and send it as response.
 	t, err := token.SignedString([]byte("secret"))
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+		res.Status = http.StatusInternalServerError
+		res.Message = err.Error()
+
+		return c.JSON(http.StatusInternalServerError, res)
 	}
 
-	return c.JSON(http.StatusOK, map[string]string{"token": t})
+	res.Status = http.StatusOK
+	res.Message = "Ok"
+	res.Data = map[string]string{"token": t}
+	return c.JSON(http.StatusOK, res)
 }
--- models/login.model.go
+++ models/login.model.go
@@ -1,22 +1,14 @@
 package models
 
-import (
-	"database/sql"
-	"fmt"
-
-	"github.com/muhammadsyazili/echo-rest/db"
-	"github.com/muhammadsyazili/echo-rest/helpers"
-)
-
+import "github.com/muhammadsyazili/echo-rest/db"
 
 type Login struct {
 	Id       int    `json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
-	Student_id string `json:"student_id"`
 }
 
-func CheckLogin(username string, password string) (bool, error) {
+func CheckLogin(username string, password string) (Login, error) {
 	var obj Login
 
 	conn := db.CreateConn()
@@ -24,23 +16,11 @@ func CheckLogin(username string, password string) (bool, error) {
 	
 	sqlQuery := "SELECT * FROM accounts WHERE username = ?"
 
-	err := conn.QueryRow(sqlQuery, username).Scan(&obj.Id, &obj.Username, &obj.Password, &obj.Student_id)
-
-	if err == sql.ErrNoRows {
-		fmt.Println("Username not found!")
-		return false, err
-	}
+	err := conn.QueryRow(sqlQuery, username).Scan(&obj.Id, &obj.Username, &obj.Password)
 
 	if err != nil {
-		return false, err
+		return obj, err
 	}
 
-	match, err := helpers.CheckHash(password, obj.Password)
-	if !match {
-		fmt.Println(obj.Password)
-		fmt.Println("Password and hash does't match!")
-		return false, err
-	}
-
-	return true, nil
+	return obj, nil
 }
